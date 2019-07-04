cf07857c82eb476c79074301131265105f0bdf3c
LNeeevs <lucasneves.dev@outlook.com>
2019-07-04 19:03:50 -0300

    FizzBuzz complete! Go to test a Calculator now!

---
+++ tests/main.spec.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import FizzBuzz from '../src/Fizzbuzz';
+
+describe('FizzBuzz Prototype', () => {
+
+  it('Should...', () => {
+    expect(FizzBuzz(3)).to.be.equal('Fizz');
+  });
+
+  it('Should...', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz');
+  });
+
+  it('Should...', () => {
+    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
+  });
+
+  it('Should...', () => {
+    expect(FizzBuzz(0)).to.be.equal(0);
+  });
+
+});
